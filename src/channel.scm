eb44ee2ded80fee2f7cba482c765ab0f71fdd1db
Artyom Poptsov <poptsov.artyom@gmail.com>
2014-01-02 23:19:13 +0400

    src/channel.scm: Update copyright notice.

--- src/channel.scm
+++ src/channel.scm
@@ -1,6 +1,6 @@
 ;;; channel.scm -- API for SSH channel manipulation.
 
-;; Copyright (C) 2013 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;; Copyright (C) 2013, 2014 Artyom V. Poptsov <poptsov.artyom@gmail.com>
 ;;
 ;; This file is a part of libguile-ssh.
 ;;
