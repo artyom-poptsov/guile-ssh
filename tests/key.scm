94abb7fa8bbd6950bd79cd676be279aabc73923b
Artyom Poptsov <poptsov.artyom@gmail.com>
2014-06-01 16:33:44 +0400

    tests/key.scm: New test suite
    * tests/key.scm: New test suite.
    * tests/Makefile.am (SCM_TESTS): Add `key.scm'.
      (EXTRA_DIST): Add `dsakey.pub', `ecdsakey', `ecdsakey.pub',
      `rsakey.pub'.
    * tests/dsakey.pub, tests/ecdsakey, tests/ecdsakey.pub,
      tests/rsakey.pub: New files
    * NEWS: Update.

---
+++ tests/key.scm
@@ -0,0 +1,91 @@
+;;; key.scm -- Testing of Guile-SSH keys
+
+;; Copyright (C) 2014 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;;
+;; This file is a part of Guile-SSH.
+;;
+;; Guile-SSH is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+;;
+;; Guile-SSH is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with Guile-SSH.  If not, see <http://www.gnu.org/licenses/>.
+
+(use-modules (srfi srfi-64)
+             (ssh session)
+             (ssh key))
+
+(define %topdir (getenv "abs_top_srcdir"))
+(define %rsa-private-key-file   (format #f "~a/tests/rsakey"   %topdir))
+(define %dsa-private-key-file   (format #f "~a/tests/dsakey"   %topdir))
+(define %ecdsa-private-key-file (format #f "~a/tests/ecdsakey" %topdir))
+(define %rsa-public-key-file    (format #f "~a/tests/rsakey.pub"   %topdir))
+(define %dsa-public-key-file    (format #f "~a/tests/dsakey.pub"   %topdir))
+(define %ecdsa-public-key-file  (format #f "~a/tests/ecdsakey.pub" %topdir))
+
+(test-begin "key")
+
+(test-assert "private-key-from-file"
+  (let ((session (%make-session)))
+    (and (private-key-from-file session %rsa-private-key-file)
+         (private-key-from-file session %dsa-private-key-file)
+         (private-key-from-file session %ecdsa-private-key-file))))
+
+(test-assert "public-key-from-file"
+  (and (public-key-from-file %rsa-public-key-file)
+       (public-key-from-file %dsa-public-key-file)
+       (public-key-from-file %ecdsa-public-key-file)))
+
+(define *session* (%make-session))
+(define *rsa-key*       (private-key-from-file *session* %rsa-private-key-file))
+(define *dsa-key*       (private-key-from-file *session* %dsa-private-key-file))
+(define *ecdsa-key*     (private-key-from-file *session* %ecdsa-private-key-file))
+(define *rsa-pub-key*   (public-key-from-file %rsa-public-key-file))
+(define *dsa-pub-key*   (public-key-from-file %dsa-public-key-file))
+(define *ecdsa-pub-key* (public-key-from-file %ecdsa-public-key-file))
+
+(test-assert "key?"
+  (and (not (key? "not a key"))
+       (key? *rsa-key*)
+       (key? *dsa-key*)
+       (key? *ecdsa-key*)
+       (key? *rsa-pub-key*)
+       (key? *dsa-pub-key*)
+       (key? *ecdsa-pub-key*)))
+
+(test-assert "private-key?"
+  (and (private-key? *rsa-key*)
+       (not (private-key? *rsa-pub-key*))
+       (not (private-key? "not a key"))))
+
+(test-assert "public-key?"
+  (and (public-key? *rsa-pub-key*)
+       ;; FIXME: Currently a SSH key that has been read from a file
+       ;; has both public and private flags.  It means that we cannot
+       ;; distinguish whether the key is private or public.
+       ;;
+       ;; So that will return `#f':
+       ;; (not (public-key? *rsa-key*))
+       (not (public-key? "not a key"))))
+
+(test-assert "private-key->public-key"
+  (and (private-key->public-key *rsa-key*)
+       (private-key->public-key *dsa-key*)
+       (private-key->public-key *ecdsa-key*)))
+
+(test-assert "get-key-type"
+  (and (eq? 'rsa   (get-key-type *rsa-key*))
+       (eq? 'dss   (get-key-type *dsa-key*))
+       (eq? 'ecdsa (get-key-type *ecdsa-key*))))
+
+(test-end "key")
+
+(exit (= (test-runner-fail-count (test-runner-current)) 0))
+
+;;; key.scm ends here.
