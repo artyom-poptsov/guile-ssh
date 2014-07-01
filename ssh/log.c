41447f78b784ca9090dca967351e1d2414a68b41
Artyom Poptsov <poptsov.artyom@gmail.com>
2014-07-02 03:39:56 +0400

    ssh/log.c: Add copyright information

--- ssh/log.c
+++ ssh/log.c
@@ -1,4 +1,22 @@
-/* log.c -- Custom logging for Guile-SSH */
+/* log.c -- Guile-SSH logging procedures
+ *
+ * Copyright (C) 2014 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+ *
+ * This file is part of Guile-SSH
+ *
+ * Guile-SSH is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Guile-SSH is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Guile-SSH.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <libguile.h>
 #include <libssh/libssh.h>
