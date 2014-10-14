93e9de9e7ed4c8aad9d9c51c32c173b57bd6939d
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2014-10-14 06:45:17 +0400

    ssh/dist/node.scm: New module
    * ssh/Makefile.am (SUBDIRS): Add `dist'.
    * ssh/dist/node.scm, ssh/dist/Makefile.am: Add to the repository.
    * configure.ac: Add `ssh/dist/Makefile'.

---
+++ ssh/dist/node.scm
@@ -0,0 +1,162 @@
+;;; node.scm -- Distributed computing node
+
+(define-module (ssh dist node)
+  #:use-module (ice-9 rdelim)
+  #:use-module (ssh session)
+  #:use-module (ssh server)
+  #:use-module (ssh message)
+  #:use-module (ssh session)
+  #:use-module (ssh channel)
+  #:use-module (ssh key)
+  #:export (node?
+            make-node
+            run-node
+            ;; Low-level procedures
+            %handle-job))
+
+(define %delimiter "\0")
+
+(define <node>
+  (make-vtable "pw"
+               (lambda (struct port)
+                 (let* ((server   (struct-ref struct 0))
+                        (bindaddr (server-get server 'bindaddr))
+                        (bindport (server-get server 'bindport)))
+                   (format port "#<node ~a:~a>"
+                           (if bindaddr
+                               bindaddr
+                               "")
+                           bindport)))))
+
+(define (node? x)
+  "Check if X is a <node> instance."
+  (and (struct? x)
+       (eq? (struct-vtable x) <node>)))
+
+(define* (make-node #:key (white-list '())
+                          (black-list '())
+                          (bindaddr   #f)
+                          (bindport   2223)
+                          (rsa-key    #f)
+                          (dsa-key    #f)
+                          (log-verbosity 'nolog))
+  (let ((server (make-server #:bindaddr      bindaddr
+                             #:bindport      bindport
+                             #:rsakey        rsa-key
+                             #:dsakey        dsa-key
+                             #:log-verbosity log-verbosity)))
+    (make-struct/no-tail <node> server)))
+
+(define (accept-and-catch server)
+  "Accept new client connection to a SERVER.  Catch errors, return #f on
+error."
+  (catch 'guile-ssh-error
+    (lambda ()
+      (server-accept server))
+    (lambda (key . args)
+      (format #t "~a: ~a~%" key args)
+      #f)))
+
+(define (read-all port)
+  "Read all lines from the PORT."
+  (let r ((res (read-line port 'concat))
+          (str ""))
+    (if (and (not (eof-object? str)) (char-ready? port))
+        (r (string-append res str) (read-line port 'concat))
+        res)))
+
+(define (handle-req-channel-open msg msg-type)
+  (let ((subtype (cadr msg-type)))
+    (format #t "  subtype: ~a~%" subtype)
+    (case subtype
+      ((channel-session)
+       (message-channel-request-open-reply-accept msg))
+      (else
+       (message-reply-default msg)
+       #f))))
+
+(define (%send-message message channel)
+  "Send MESSAGE to CHANNEL."
+  (write message channel)
+  (write-char #\nul channel))
+
+(define (%recv-message channel)
+  "Receive server response from the CHANNEL."
+  (read-delimited %delimiter channel))
+
+(define (%handle-job channel)
+  "Receive and handle distributed job from CHANNEL."
+  (let* ((data (read (open-input-string (%recv-message channel))))
+         (proc (primitive-eval (car data))))
+    (format #t "proc: ~a; list: ~a~%" proc (cadr data))
+    (%send-message (map proc (cadr data)) channel)))
+
+(define (handle-req-auth session msg msg-type)
+  (let ((subtype (cadr msg-type)))
+
+    (format #t "  subtype: ~a~%" subtype)
+
+    ;; Allowed authentication methods
+    (message-auth-set-methods! msg '(public-key))
+
+    (case subtype
+      ((auth-method-publickey)
+       (let* ((req          (message-get-req msg))
+              (user         (auth-req:user req))
+              (pubkey       (auth-req:pubkey req))
+              (pubkey-state (auth-req:pubkey-state req)))
+         (format #t
+                 (string-append "  User ~a wants to authenticate with a public key (~a)~%"
+                                "  Public key state: ~a~%")
+                 user (get-key-type pubkey) pubkey-state)
+
+         (case pubkey-state
+           ((none)
+            (message-auth-reply-public-key-ok msg))
+
+           ((valid)
+            (message-reply-success msg))
+
+           (else
+            (format #t "  Bad public key state: ~a~%" pubkey-state)
+            (message-reply-default msg)))))
+
+      (else
+       (message-reply-default msg)))))
+
+(define (run-session-loop session)
+  (display "Session loop\n")
+  (while #t
+    (let ((msg (server-message-get session)))
+      (format #t "Message: ~a~%" msg)
+      (and msg
+           (let ((msg-type (message-get-type msg)))
+             (format #t "Message: ~a~%" msg-type)
+             (case (car msg-type)
+               ((request-service)
+                (message-reply-success msg))
+               ((request-auth)
+                (handle-req-auth session msg msg-type))
+               ((request-channel-open)
+                (let* ((channel (handle-req-channel-open msg msg-type)))
+                  (%handle-job channel)
+                  (close channel)
+                  (break)))
+               (else
+                (message-reply-default msg))))))))
+
+(define (run-node node)
+  (let ((server (struct-ref node 0)))
+    (server-listen server)
+    (while #t
+      (let ((session (accept-and-catch server)))
+        (format #t "Session: ~a~%" session)
+        (or session
+            (begin
+              (sleep 1)
+              (continue)))
+        (server-handle-key-exchange session)
+        (run-session-loop session)
+        (disconnect! session)))))
+
+;;; node.scm ends here
