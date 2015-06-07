f84157cbed4a02c9318186eb76d63abe8edc8c5d
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2015-06-07 09:50:48 +0300

    ssh/dist.scm: Implement <job> type
    * ssh/dist.scm: Implement <job> type.
      (dist-map): Use the new type.
      (%split-job): Rename to '%assign-jobs'.
      (%hand-out-jobs): Rename to '%hand-out-job'.  Handle only one job.

--- ssh/dist.scm
+++ ssh/dist.scm
@@ -31,6 +31,7 @@
   #:use-module (ice-9 iconv)
   #:use-module (ice-9 rdelim)
   #:use-module (ice-9 receive)
+  #:use-module (srfi srfi-9 gnu)
   #:use-module (srfi srfi-1)
   #:use-module (srfi srfi-26)
   #:use-module (ssh dist node)
@@ -39,11 +40,30 @@
   #:export (dist-map
 
             ;; Low-level procedures
+            %make-job
             %flatten-1
             %split
-            %split-job
-            %hand-out-jobs))
+            %assign-jobs
+            %hand-out-job))
 
+
+(define-immutable-record-type <job>
+  (%make-job type node data proc)
+  job?
+  (type job-type)
+  (node job-node)
+  (data job-data)
+  (proc job-proc))
+
+(set-record-type-printer!
+ <job>
+ (lambda (job port)
+   (format port "#<job ~a ~a ~a>"
+           (job-type job)
+           (job-node job)
+           (number->string (object-address job) 16))))
+
+
 (define (%flatten-1 lst)
   "Flatten a list LST one level down.  Return a flattened list."
   (fold-right append '() lst))
@@ -68,27 +88,25 @@
               (loop l (1- n) (append res (list l))))
           res))))
 
-(define (%split-job nodes lst)
-  "Split job to nearly equal parts according to length of NODES list.  Return
-list of jobs."
-  (map (cut list <> <>)
+(define (%assign-jobs nodes lst proc)
+  "Split the work to nearly equal parts according to length of NODES list and
+assign each part of work to a node.  Return list of assigned jobs."
+  (map (cut %make-job 'map <> <> proc)
        nodes
        (%split lst (length nodes))))
 
-(define (%hand-out-jobs jobs proc)
-  "Hand out JOBS to nodes and return the result of computation."
-  (map-in-order (lambda (job)
-                  (node-eval (car job)
-                             `(,(quote map) ,proc (quote ,(cadr job)))))
-                jobs))
+(define (%hand-out-job job)
+  "Hand out JOB to the assigned node and return the result of computation."
+  (node-eval (job-node job)
+             `(,(job-type job) ,(job-proc job) (quote ,(job-data job)))))
 
 
 (define-syntax-rule (dist-map nodes proc lst)
   "Do list mapping using distributed computation.  The job is splitted to
 nearly equal parts and hand out resulting jobs to NODES.  Return the result of
 computation."
-    (let ((jobs (%split-job nodes lst)))
-      (%flatten-1 (%hand-out-jobs jobs (quote proc)))))
+    (let ((jobs (%assign-jobs nodes lst (quote proc))))
+      (%flatten-1 (map %hand-out-job jobs))))
 
 ;;; dist.scm ends here
 
