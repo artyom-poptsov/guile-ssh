3f98878b4cb0eed1bb89de59d321446f68b7c7d4
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2015-07-19 11:14:57 +0300

    ssh/dist/node.scm: Add the copyright

--- ssh/dist/node.scm
+++ ssh/dist/node.scm
@@ -1,5 +1,30 @@
 ;;; node.scm -- Distributed computing node
 
+;; Copyright (C) 2015 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;;
+;; This file is a part of Guile-SSH.
+;;
+;; Guile-SSH is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+;;
+;; Guile-SSH is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with Guile-SSH.  If not, see
+;; <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+
+
+;;; Code:
+
 (define-module (ssh dist node)
   #:use-module (ice-9 rdelim)
   #:use-module (ice-9 regex)
