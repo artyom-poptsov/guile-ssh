7936dc62f0524229ede139375984a417e3ddd04c
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2015-10-29 15:08:08 +0300

    tests/server.scm: Update copyright dates

--- tests/server.scm
+++ tests/server.scm
@@ -1,6 +1,6 @@
 ;;; server.scm -- Testing of server procedures without a client.
 
-;; Copyright (C) 2014 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;; Copyright (C) 2014, 2015 Artyom V. Poptsov <poptsov.artyom@gmail.com>
 ;;
 ;; This file is a part of Guile-SSH.
 ;;
