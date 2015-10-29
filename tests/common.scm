cae9521e0301d3135f3ee70de83ed19dec21a757
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2015-10-29 15:39:41 +0300

    tests: Move keys to 'tests/keys' directory
    * tests/rsakey, tests/rsakey.pub, tests/dsakey, tests/dsakey.pub,
      tests/ecdsakey, tests/ecdsakey.pub,: Move to 'tests/keys' directory.
    * tests/common.scm: Update pathes to keys.
    * tests/Makefile.am (EXTRA_DIST): Update.

--- tests/common.scm
+++ tests/common.scm
@@ -57,12 +57,12 @@
 
 
 ;; Keys
-(define %rsakey       (format #f "~a/tests/rsakey"       %topdir))
-(define %rsakey-pub   (format #f "~a/tests/rsakey.pub"   %topdir))
-(define %dsakey       (format #f "~a/tests/dsakey"       %topdir))
-(define %dsakey-pub   (format #f "~a/tests/dsakey.pub"   %topdir))
-(define %ecdsakey     (format #f "~a/tests/ecdsakey"     %topdir))
-(define %ecdsakey-pub (format #f "~a/tests/ecdsakey.pub" %topdir))
+(define %rsakey       (format #f "~a/tests/keys/rsakey"       %topdir))
+(define %rsakey-pub   (format #f "~a/tests/keys/rsakey.pub"   %topdir))
+(define %dsakey       (format #f "~a/tests/keys/dsakey"       %topdir))
+(define %dsakey-pub   (format #f "~a/tests/keys/dsakey.pub"   %topdir))
+(define %ecdsakey     (format #f "~a/tests/keys/ecdsakey"     %topdir))
+(define %ecdsakey-pub (format #f "~a/tests/keys/ecdsakey.pub" %topdir))
 
 
 (define %knownhosts (format #f "~a/tests/knownhosts"
