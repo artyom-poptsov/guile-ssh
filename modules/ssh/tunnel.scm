5602a7aa32b0049d828f0d77fa3476c48e2fd667
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2017-05-08 11:15:36 +0300

    modules/ssh/tunnel.scm: Update copyright dates

--- modules/ssh/tunnel.scm
+++ modules/ssh/tunnel.scm
@@ -1,6 +1,6 @@
 ;;; tunnel.scm -- SSH tunnels
 
-;; Copyright (C) 2015, 2016 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;; Copyright (C) 2015, 2016, 2017 Artyom V. Poptsov <poptsov.artyom@gmail.com>
 ;;
 ;; This file is a part of Guile-SSH.
 ;;
