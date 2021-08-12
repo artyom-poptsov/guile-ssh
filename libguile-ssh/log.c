452a9190ae597ebce8ccb59b7b310ddfa57fd3e0
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-08-12 22:58:32 +0300

    libguile-ssh/log.c (guile_ssh_write_log): Bugfix
    * libguile-ssh/log.c (guile_ssh_write_log): Bugfix: Use dynwind to free
      allocated C strings.

--- libguile-ssh/log.c
+++ libguile-ssh/log.c
@@ -194,6 +194,8 @@ undefined. \
   char *c_function_name;
   char *c_message;
 
+  scm_dynwind_begin (0);
+
   SCM_ASSERT (scm_symbol_p (priority),      priority,      SCM_ARG1, FUNC_NAME);
   SCM_ASSERT (scm_string_p (function_name), function_name, SCM_ARG2, FUNC_NAME);
   SCM_ASSERT (scm_string_p (message),       message,       SCM_ARG3, FUNC_NAME);
@@ -203,9 +205,14 @@ undefined. \
     guile_ssh_error1 (FUNC_NAME, "Wrong priority level", priority);
 
   c_function_name = scm_to_locale_string (function_name);
+  scm_dynwind_free (c_function_name);
+
   c_message       = scm_to_locale_string (message);
+  scm_dynwind_free (c_message);
 
   _ssh_log (c_priority->value, c_function_name, "%s", c_message);
+
+  scm_dynwind_end ();
 
   return SCM_UNDEFINED;
 }
