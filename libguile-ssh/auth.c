6982f0c1f19c1a72d18e819338434e2f865aadd6
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-08-14 10:58:26 +0300

    libguile-ssh/auth.c: Cleanup

--- libguile-ssh/auth.c
+++ libguile-ssh/auth.c
@@ -1,6 +1,6 @@
 /* auth.c -- User authentication procedures.
  *
- * Copyright (C) 2013-2020 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+ * Copyright (C) 2013-2021 Artyom V. Poptsov <poptsov.artyom@gmail.com>
  *
  * This file is part of Guile-SSH.
  *
@@ -72,26 +72,21 @@ ssh_auth_result_to_symbol (const int res)
 SCM_DEFINE (guile_ssh_userauth_public_key_x, "userauth-public-key!", 2, 0, 0,
             (SCM session_smob,
              SCM private_key_smob),
-            "\
-Try to authenticate with a public key.\n\
-Throw `wrong-type-arg' if a disconnected SESSION is passed as an argument.\
-")
+            "Try to authenticate with a public key.\n"
+            "Throw `wrong-type-arg' if a disconnected SESSION is passed\n"
+            "as an argument.")
 #define FUNC_NAME s_guile_ssh_userauth_public_key_x
 {
   gssh_session_t *session_data = gssh_session_from_scm (session_smob);
   gssh_key_t *private_key_data = gssh_key_from_scm (private_key_smob);
-
-  /* See "On the username" commentary above. */
-  char *username = NULL;
-
   int res;
 
-  /* Check types. */
   GSSH_VALIDATE_CONNECTED_SESSION (session_data, session_smob, SCM_ARG1);
   SCM_ASSERT (_private_key_p (private_key_data),
               private_key_smob, SCM_ARG2, FUNC_NAME);
 
-  res = ssh_userauth_publickey (session_data->ssh_session, username,
+  res = ssh_userauth_publickey (session_data->ssh_session,
+                                NULL, /* username */
                                 private_key_data->ssh_key);
 
   return ssh_auth_result_to_symbol (res);
@@ -101,25 +96,20 @@ Throw `wrong-type-arg' if a disconnected SESSION is passed as an argument.\
 SCM_DEFINE (guile_ssh_userauth_public_key_auto_x,
             "userauth-public-key/auto!", 1, 0, 0,
             (SCM session),
-            "\
-Try to automatically authenticate with \"none\" method first and then with \n\
-public keys.  If the key is encrypted the user will be asked for a \n\
-passphrase.  Return one of the following symbols: error, denied, partial, \n\
-success.\n\
-\n\
-Throw `wrong-type-arg' if a disconnected SESSION is passed as an argument.\
-")
+            "Try to automatically authenticate with \"none\" method first\n"
+            "and then with public keys.  If the key is encrypted the user\n"
+            "will be asked for a passphrase.  Return one of the following \n"
+            "symbols: error, denied, partial, success.\n"
+            "Throw `wrong-type-arg' if a disconnected SESSION is passed\n"
+            "as an argument.")
 #define FUNC_NAME s_guile_ssh_userauth_public_key_auto_x
 {
   gssh_session_t *sd = gssh_session_from_scm (session);
-  char *username = NULL; /* See "On the username" commentary above. */
-  char *passphrase = NULL;
-
   GSSH_VALIDATE_CONNECTED_SESSION (sd, session, SCM_ARG1);
 
   int res = ssh_userauth_publickey_auto (sd->ssh_session,
-                                         username,
-                                         passphrase); /* passphrase */
+                                         NULL,        /* username */
+                                         NULL);       /* passphrase */
   return ssh_auth_result_to_symbol (res);
 }
 #undef FUNC_NAME
@@ -134,7 +124,6 @@ Throw `wrong-type-arg' if a disconnected SESSION is passed as an argument.\
 {
   gssh_session_t *sd = gssh_session_from_scm (session);
   gssh_key_t *kd = gssh_key_from_scm (public_key);
-  char *username = NULL;        /* See "On the username" commentary above */
   int res;
 
   GSSH_VALIDATE_CONNECTED_SESSION (sd, session, SCM_ARG1);
@@ -143,7 +132,9 @@ Throw `wrong-type-arg' if a disconnected SESSION is passed as an argument.\
   if (! ssh_is_connected (sd->ssh_session))
     guile_ssh_error1 (FUNC_NAME, "Session is not connected", session);
 
-  res = ssh_userauth_try_publickey (sd->ssh_session, username, kd->ssh_key);
+  res = ssh_userauth_try_publickey (sd->ssh_session,
+                                    NULL, /* username */
+                                    kd->ssh_key);
   return ssh_auth_result_to_symbol (res);
 }
 #undef FUNC_NAME
@@ -151,20 +142,17 @@ Throw `wrong-type-arg' if a disconnected SESSION is passed as an argument.\
 SCM_DEFINE (guile_ssh_userauth_agent_x,
             "userauth-agent!", 1, 0, 0,
             (SCM session),
-            /* FIXME: Fix the docsring. */
-            "\
-Throw `wrong-type-arg' if a disconnected SESSION is passed as an argument.\
-")
+            "Throw `wrong-type-arg' if a disconnected SESSION is passed"
+            " as an argument.")
 #define FUNC_NAME s_guile_ssh_userauth_agent_x
 {
   gssh_session_t *sd = gssh_session_from_scm (session);
-
-  char *username = NULL; /* See "On the username" commentary above. */
   int res;
 
   GSSH_VALIDATE_CONNECTED_SESSION (sd, session, SCM_ARG1);
 
-  res = ssh_userauth_agent (sd->ssh_session, username);
+  res = ssh_userauth_agent (sd->ssh_session,
+                            NULL /* username */ );
 
   return ssh_auth_result_to_symbol (res);
 }
@@ -173,17 +161,12 @@ Throw `wrong-type-arg' if a disconnected SESSION is passed as an argument.\
 /* Try to authenticate by password. */
 SCM_DEFINE (guile_ssh_userauth_password_x, "userauth-password!", 2, 0, 0,
             (SCM session, SCM password),
-            "\
-Try to authenticate by password.\n\
-Throw `wrong-type-arg' if a disconnected SESSION is passed as an argument.\
-")
+            "Try to authenticate by password.\n"
+            "Throw `wrong-type-arg' if a disconnected SESSION is passed"
+            " as an argument.")
 #define FUNC_NAME s_guile_ssh_userauth_password_x
 {
   gssh_session_t* session_data = gssh_session_from_scm (session);
-
-  /* See "On the username" commentary above. */
-  char *username = NULL;
-
   char *c_password;
   int res;
 
@@ -197,7 +180,7 @@ Throw `wrong-type-arg' if a disconnected SESSION is passed as an argument.\
   scm_dynwind_free (c_password);
 
   res = ssh_userauth_password (session_data->ssh_session,
-                               username,
+                               NULL, /* username */
                                c_password);
 
   scm_dynwind_end ();
@@ -209,10 +192,9 @@ Throw `wrong-type-arg' if a disconnected SESSION is passed as an argument.\
 SCM_DEFINE (guile_ssh_userauth_gssapi_x,
             "userauth-gssapi!", 1, 0, 0,
             (SCM session),
-            "\
-Try to authenticate through the \"gssapi-with-mic\" method.\
-Throw `wrong-type-arg' if a disconnected SESSION is passed as an argument.\
-")
+            "Try to authenticate through the \"gssapi-with-mic\" method."
+            "Throw `wrong-type-arg' if a disconnected SESSION is passed"
+            " as an argument.")
 #define FUNC_NAME s_guile_ssh_userauth_gssapi_x
 {
   gssh_session_t *sd = gssh_session_from_scm (session);
@@ -234,10 +216,9 @@ Throw `wrong-type-arg' if a disconnected SESSION is passed as an argument.\
    'partial, 'again */
 SCM_DEFINE (guile_ssh_userauth_none_x, "userauth-none!", 1, 0, 0,
             (SCM arg1),
-            "\
-Try to authenticate through the \"none\" method.\n\
-Throw `wrong-type-arg' if a disconnected SESSION is passed as an argument.\
-")
+            "Try to authenticate through the \"none\" method.\n"
+            "Throw `wrong-type-arg' if a disconnected SESSION is passed"
+            " as an argument.")
 #define FUNC_NAME s_guile_ssh_userauth_none_x
 {
   gssh_session_t *session_data = gssh_session_from_scm (arg1);
@@ -245,9 +226,8 @@ Throw `wrong-type-arg' if a disconnected SESSION is passed as an argument.\
 
   GSSH_VALIDATE_CONNECTED_SESSION (session_data, arg1, SCM_ARG1);
 
-  /* username is deprecated parameter.  Should be set to NULL. */
   res = ssh_userauth_none (session_data->ssh_session,
-                           NULL); /* Username */
+                           NULL /* username */ );
 
   return ssh_auth_result_to_symbol (res);
 }
@@ -258,10 +238,9 @@ Throw `wrong-type-arg' if a disconnected SESSION is passed as an argument.\
    Return list of available methods. */
 SCM_DEFINE (guile_ssh_userauth_get_list, "userauth-get-list", 1, 0, 0,
             (SCM session),
-            "\
-Get available authentication methods for a session SESSION.\n\
-Throw `wrong-type-arg' if a disconnected SESSION is passed as an argument.\
-")
+            "Get available authentication methods for a session SESSION.\n"
+            "Throw `wrong-type-arg' if a disconnected SESSION is passed "
+            " as an argument.")
 #define FUNC_NAME s_guile_ssh_userauth_get_list
 {
   gssh_session_t *session_data = gssh_session_from_scm (session);
@@ -270,10 +249,8 @@ Throw `wrong-type-arg' if a disconnected SESSION is passed as an argument.\
 
   GSSH_VALIDATE_CONNECTED_SESSION (session_data, session, SCM_ARG1);
 
-  /* The second argument of the function is a username.  According to
-     the documentation for libssh 0.5.3, this argument is deprecated
-     and must be set to NULL. */
-  res = ssh_userauth_list (session_data->ssh_session, NULL);
+  res = ssh_userauth_list (session_data->ssh_session,
+                           NULL /* username */ );
 
   if (res & SSH_AUTH_METHOD_PASSWORD)
     {
