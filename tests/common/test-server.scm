b62bc12c91ec901bef61ac54b082ba7547c5328e
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-08-14 09:51:28 +0300

    tests/common/test-server.scm: Remove debug traces

--- tests/common/test-server.scm
+++ tests/common/test-server.scm
@@ -19,14 +19,14 @@
     (close f)))
 
 (define (main args)
-  (log args)
+  ;; (log args)
+  (usleep 100)
   (let ((test-suite-name (list-ref args 1))
         (test-name       (list-ref args 2)))
     (unless (file-exists? test-suite-name)
       (mkdir test-suite-name))
     (set-log-userdata! test-name)
     (setup-test-suite-logging! (string-append test-suite-name "/" test-name))
-    (log "test 1")
     (let* ((port      (string->number (list-ref args 3)))
            (handler   (eval-string (list-ref args 4)))
            (s         (make-server
@@ -35,10 +35,8 @@
                        #:rsakey   %rsakey
                        #:dsakey   %dsakey
                        #:log-verbosity 'functions)))
-      (log "test 2")
       (server-listen s)
       (let ((p (open-output-file (string-append test-name ".run"))))
         (close p))
       (log handler)
-      (handler s)   (log "test 4"))))
-
+      (handler s))))
