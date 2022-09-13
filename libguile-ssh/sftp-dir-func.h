a5d631f3deae9c0cf85e9ab9617c8546f7f7ff10
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2022-09-13 11:49:41 +0300

    libguile-ssh/sftp-dir-func.h: Add

---
+++ libguile-ssh/sftp-dir-func.h
@@ -0,0 +1,24 @@
+/* Copyright (C) 2022 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+ *
+ * This file is part of Guile-SSH.
+ *
+ * Guile-SSH is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Guile-SSH is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Guile-SSH.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __SFTP_DIR_FUNC_H__
+#define __SFTP_DIR_FUNC_H__
+
+void init_sftp_dir_func (void);
+
+#endif  /* ifndef __SFTP_DIR_FUNC_H__ */
